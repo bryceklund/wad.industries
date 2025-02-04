58c53c5fd2886fd379b626b436d9e6f0eccdfd66
bryce <bryce.eklund@gmail.com>
2025-02-03 20:23:55 -0800

    Checkpoint before refactor of current conversion state

--- src/components/LogoOverlay.jsx
+++ src/components/LogoOverlay.jsx
@@ -1,24 +1,17 @@
-// src/components/LogoOverlayGrid.jsx
-import { GridAlignedOverlay } from './GridAlignedMedia';
 import AsciiElement from './AsciiElement';
 import styles from '/src/styles/LogoOverlay.module.css';
 
-function LogoOverlayGrid() {
-  return (
-    <div className={styles.container}>
-      <GridAlignedOverlay
-        imageSrc="/src/assets/logo-glow.png"
-        imageAlt="WAD Industries Logo"
-        className={styles.imageWrapper}
-      >
-        <AsciiElement 
-          className={styles.asciiWrapper} 
-          style={styles.asciiArt} 
-          asset='wadIndustriesSimple' 
-        />
-      </GridAlignedOverlay>
-    </div>
-  );
+function LogoOverlay() {
+    return (
+        <div className={styles.container} >
+            <div className={styles.imageWrapper} >
+                <img className={styles.image} src='/src/assets/logo-glow.png' />
+                <AsciiElement className={styles.asciiWrapper} style={styles.asciiArt} asset='wadIndustriesSimple' />
+            </div>
+        </div>
+        
+    );
 }
 
-export default LogoOverlayGrid;
+export default LogoOverlay;
+
